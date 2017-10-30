b9b4dd6a0442686f6baf69afc5476cf52ee73a1b
Nick <nickolay.sadovoy@gmail.com>
2017-10-30 10:29:50 +0200

    Add Tests.Core project

---
+++ src/UI.AutomationTests.UnitTests.Core/AutomationElementExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Automation;
+
+namespace UI.AutomationTests.UnitTests.Core
+{
+    public static class AutomationElementExtensions
+    {
+        public static void SetValueAsValuePattern(this AutomationElement automationElement, string value)
+        {
+            ValuePattern valuePattern = automationElement.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+            valuePattern.SetValue(value);
+        }
+
+        public static string GetValueAsValuePattern(this AutomationElement automationElement)
+        {
+            ValuePattern valuePattern = automationElement.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+            return valuePattern.Current.Value;
+        }
+
+        public static void InvokeAsInvokePattern(this AutomationElement automationElement)
+        {
+            var invokePattern = automationElement.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            invokePattern.Invoke();
+        }
+
+        public static AutomationElement GetControlElement(this AutomationElement parentElement, object value, AutomationProperty property = null, PropertyConditionFlags flags = PropertyConditionFlags.IgnoreCase, TreeScope treeScope = TreeScope.Descendants)
+        {
+            if (property == null)
+                property = AutomationElement.AutomationIdProperty;
+
+            Condition condition = new PropertyCondition(property, value, flags);
+            AutomationElement controlElement = parentElement.FindFirst(treeScope, condition);
+            return controlElement;
+        }
+    }
+}
